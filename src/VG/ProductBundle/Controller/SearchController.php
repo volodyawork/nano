0c3bf919fd18ec4b031c6d784fcc4585469ebf5e
Vladimir <volodyawork@gmail.com>
2015-03-15 23:12:52 +0200

    remove elasticsearch

--- src/VG/ProductBundle/Controller/SearchController.php
+++ src/VG/ProductBundle/Controller/SearchController.php
@@ -2,6 +2,8 @@
 
 namespace VG\ProductBundle\Controller;
 
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\Common\Util\Debug;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
@@ -76,6 +78,76 @@ class SearchController extends Controller
         return
             array(
                 'results' => $elasticaResultSet,
+                'query' => $query,
+                'page_num' => $page,
+                'page_count' => $last_page,
+                'last_page' => $last_page,
+                'previous_page' => $previous_page,
+                'current_page' => $page,
+                'next_page' => $next_page,
+                'total' => $total
+            );
+    }
+
+    /**
+     *
+     * @Route("/search2", name="search2")
+     * @Method("GET")
+     * @Template()
+     */
+    public function search2Action()
+    {
+        $request = $this->container->get('request_stack')->getCurrentRequest();
+        $query = $request->get('s', '*');
+        if ('' === $query) {
+            $query = '*';
+        }
+        $page = $request->get('page_num', 1);
+
+        $em = $this->getDoctrine()->getManager();
+
+
+        $countQuery = $em->getRepository('VGProductBundle:Product')->createQueryBuilder('p');
+        $countQuery->select('COUNT(p)');
+        if ($query != '*'){
+            $countQuery = $countQuery->where('p.status = 1 AND p.name LIKE :input')
+                ->setParameters(array(
+                    'input' => "%".$query."%"));
+        } else {
+            $countQuery = $countQuery->where('p.status = 1');
+        }
+
+        $total = $countQuery->getQuery()->getSingleScalarResult();
+        $per_page = $this->container->getParameter('max_products_on_listpage');
+        $offset = ($page - 1) * $per_page;
+
+        $qb = $em->getRepository('VGProductBundle:Product')->createQueryBuilder('p');
+        if ($query != '*'){
+            $qb = $qb->where('p.status = 1 AND p.name LIKE :input')
+                ->setParameters(array(
+                    ':input' => "%".$query."%"));
+        } else {
+            $qb = $qb->where('p.status = 1');
+        }
+        if ($per_page) {
+            $qb = $qb->setFirstResult($offset);
+            $qb = $qb->setMaxResults($per_page);
+        }
+
+            $qb = $qb->orderBy('p.id', 'DESC')
+            ->getQuery();
+
+
+        $entities = $qb->getResult();
+
+
+        $last_page = ceil($total / $per_page);
+        $previous_page = $page > 1 ? $page - 1 : 1;
+        $next_page = $page < $last_page ? $page + 1 : $last_page;
+
+        return
+            array(
+                'results' => $entities,
                 'query' => $query,
                 'page_num' => $page,
                 'page_count' => $last_page,
